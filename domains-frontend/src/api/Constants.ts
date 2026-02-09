0e39609802bf64621f22eb45ff3ed3357baa6451
pluxury37077 <pluxury37077@gmail.com>
2026-02-09 16:29:49 +0300

    fix http paths

--- domains-frontend/src/api/Constants.ts
+++ domains-frontend/src/api/Constants.ts
@@ -1,4 +1,4 @@
-export const AUTH_URL ${DB_USER:***REMOVED***} '/api/auth';
+export const AUTH_URL ${DB_USER:***REMOVED***} '/api';
 export const DOMAIN_ORDER_URL ${DB_USER:***REMOVED***} '/api/domains';
 export const EXDNS_URL ${DB_USER:***REMOVED***} '/exdns';
 export const EXDNS_TOKEN ${DB_USER:***REMOVED***} 'changeme';
